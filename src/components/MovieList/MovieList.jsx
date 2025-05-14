caad470012b715fac5b9d6f592da4553c5ad0ce9
Denis Kasatskiy <azartofgod@gmail.com>
2025-05-14 18:33:53 +0300

    Fix last solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,10 +1,16 @@
 import './MovieList.scss';
 import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MovieList = ({ movies }) => (
-  <div className="movies">
-    {movies.map((movie) => (
-      <MovieCard movie={movie} />
-    ))}
-  </div>
-);
+export const MovieList = ({ movies }) => {
+  if (movies.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="movies">
+      {movies.map(movie => (
+        <MovieCard movie={movie} key={movie.imbId}/>
+      ))}
+    </div>
+  )
+};
