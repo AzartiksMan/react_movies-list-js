41596c6afda45abbe712dee288a9598561b47487
Denis Kasatskiy <azartofgod@gmail.com>
2025-05-14 19:52:44 +0300

    Fix key

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -9,8 +9,8 @@ export const MovieList = ({ movies }) => {
   return (
     <div className="movies">
       {movies.map(movie => (
-        <MovieCard movie={movie} key={movie.imbId}/>
+        <MovieCard movie={movie} key={movie.imdbId} />
       ))}
     </div>
-  )
+  );
 };
